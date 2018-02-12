54e7a4d7227836a2aba397e468b187284ade6179
arendu <adithya.r@gmail.com>
2018-02-12 16:14:48 -0500

    use external config for paths

--- preprocess.sh
+++ preprocess.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+source ./project.config
 if [ $# -eq 1 ]
 then
   echo $1 
@@ -8,9 +9,9 @@ else
   exit 1
 fi
 source /home/arenduc1/virtualenvs/py3venv/bin/activate
-OPENMNTPATH=/home/arenduc1/virtualenvs/tools/OpenNMT-py-sding
+#OPENMNTPATH=/home/arenduc1/virtualenvs/tools/OpenNMT-py-sding
 export PYTHONPATH=$PYTHONPATH:$OPENMNTPATH/onmt/modules
-PROJECT_DIR=/export/b07/arenduc1/e2e-speech
+#PROJECT_DIR=/export/b07/arenduc1/e2e-speech
 DATA_TYPE=$1
 python $OPENMNTPATH/prepare_aug_data.py --folder $PROJECT_DIR/data/${DATA_TYPE}/ --prefix_list $DATA_TYPE.noise ${DATA_TYPE}.rep --prefix_out ${DATA_TYPE}
 #python $OPENMNTPATH/preprocess.py -train_aug_src $PROJECT_DIR/data/${DATA_TYPE}/${DATA_TYPE}.aug.train.src -train_aug_tgt $PROJECT_DIR/data/${DATA_TYPE}/${DATA_TYPE}.aug.train.tgt -train_audio_tgt $PROJECT_DIR/data/${DATA_TYPE}/${DATA_TYPE}.audio.train.tgt -valid_tgt $PROJECT_DIR/data/${DATA_TYPE}/${DATA_TYPE}.audio.valid.tgt -save_data $PROJECT_DIR/data/${DATA_TYPE}/${DATA_TYPE} -start_idx 1
--- train.sh
+++ train.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
-source /home/arenduc1/virtualenvs/py3venv/bin/activate
-
-if [ $# -eq 6 ]
+source ./project.config
+source $PYENV #/home/arenduc1/virtualenvs/py3venv/bin/activate
+if [ $# -eq 7 ]
 then
-  echo $1 $2 $3 $4 $5 $6
+  echo $1 $2 $3 $4 $5 $6 $7
 else
-  echo "invalid number of args (expected 6)"
-  echo "DATA_TYPE RNN_SIZE TRAIN_WITH_AUG DEC_LAYERS NUM_CONCAT_FLAGS ADD_NOISE"
+  echo "invalid number of args (expected 7)"
+  echo "DATA_TYPE RNN_SIZE TRAIN_WITH_AUG DEC_LAYERS NUM_CONCAT_FLAGS ADD_NOISE HIGHWAY_CONCAT"
   exit 1
 fi
 DATA_TYPE=$1
@@ -16,11 +16,12 @@ TRAIN_WITH_AUG=$3
 DEC_LAYERS=$4
 NUM_CONCAT_FLAGS=$5
 ADD_NOISE=$6
-OPENMNTPATH=/home/arenduc1/virtualenvs/tools/OpenNMT-py-sding
+HIGHWAY_CONCAT=$7
+#OPENMNTPATH=/home/arenduc1/virtualenvs/tools/OpenNMT-py-sding
 export PYTHONPATH=$PYTHONPATH:$OPENMNTPATH/onmt/modules
 
-PROJECT_DIR=/export/b07/arenduc1/e2e-speech
+#PROJECT_DIR=/export/b07/arenduc1/e2e-speech
 MODEL_DIR=$PROJECT_DIR/models/${DATA_TYPE}
 mkdir -p $MODEL_DIR
 
-python $OPENMNTPATH/train.py -data $PROJECT_DIR/data/${DATA_TYPE}/${DATA_TYPE} -save_model $MODEL_DIR/${NAME} -encoder_type hybrid -rnn_size $RNN_SIZE -word_vec_size 123 -batch_size 64  -optim adadelta -dropout 0.1 -enc_layers 4 -dec_layers $DEC_LAYERS -learning_rate_decay 0.99 -epochs 50 -train_with_aug $TRAIN_WITH_AUG -num_concat_flags $NUM_CONCAT_FLAGS -add_noise $ADD_NOISE 
+python $OPENMNTPATH/train.py -data $PROJECT_DIR/data/${DATA_TYPE}/${DATA_TYPE} -save_model $MODEL_DIR/${NAME} -encoder_type hybrid -decoder_type hybrid -rnn_size $RNN_SIZE -word_vec_size 123 -batch_size 64  -optim adadelta -dropout 0.1 -enc_layers 4 -dec_layers $DEC_LAYERS -learning_rate_decay 0.99 -epochs 50 -train_with_aug $TRAIN_WITH_AUG -num_concat_flags $NUM_CONCAT_FLAGS -add_noise $ADD_NOISE -use_highway_concat $HIGHWAY_CONCAT
