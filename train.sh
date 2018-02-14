5f40f3649d9ac2f518dd2ce72d8e338564aff110
arendu <adithya.r@gmail.com>
2018-02-14 12:02:35 -0500

    mix_factor and mix_factor_decay is now a cmd line arg

--- train.sh
+++ train.sh
@@ -2,12 +2,12 @@
 set -e
 source ./project.config
 source $PYENV #/home/arenduc1/virtualenvs/py3venv/bin/activate
-if [ $# -eq 7 ]
+if [ $# -eq 8 ]
 then
-  echo $1 $2 $3 $4 $5 $6 $7
+  echo $1 $2 $3 $4 $5 $6 $7 $8
 else
-  echo "invalid number of args (expected 7)"
-  echo "DATA_TYPE RNN_SIZE TRAIN_WITH_AUG DEC_LAYERS NUM_CONCAT_FLAGS ADD_NOISE HIGHWAY_CONCAT"
+  echo "invalid number of args (expected 8)"
+  echo "DATA_TYPE RNN_SIZE TRAIN_WITH_AUG DEC_LAYERS NUM_CONCAT_FLAGS ADD_NOISE HIGHWAY_CONCAT MIX_FACTOR"
   exit 1
 fi
 DATA_TYPE=$1
@@ -17,6 +17,7 @@ DEC_LAYERS=$4
 NUM_CONCAT_FLAGS=$5
 ADD_NOISE=$6
 HIGHWAY_CONCAT=$7
+MIX_FACTOR=$8
 #OPENMNTPATH=/home/arenduc1/virtualenvs/tools/OpenNMT-py-sding
 export PYTHONPATH=$PYTHONPATH:$OPENMNTPATH/onmt/modules
 
@@ -24,4 +25,4 @@ export PYTHONPATH=$PYTHONPATH:$OPENMNTPATH/onmt/modules
 MODEL_DIR=$PROJECT_DIR/models/${DATA_TYPE}
 mkdir -p $MODEL_DIR
 
-python $OPENMNTPATH/train.py -data $PROJECT_DIR/data/${DATA_TYPE}/${DATA_TYPE} -save_model $MODEL_DIR/${NAME} -encoder_type hybrid -rnn_size $RNN_SIZE -word_vec_size 123 -batch_size 128  -optim adadelta -dropout 0.1 -enc_layers 4 -dec_layers $DEC_LAYERS -learning_rate_decay 0.99 -epochs 50 -train_with_aug $TRAIN_WITH_AUG -num_concat_flags $NUM_CONCAT_FLAGS -add_noise $ADD_NOISE -use_highway_concat $HIGHWAY_CONCAT
+python $OPENMNTPATH/train.py -data $PROJECT_DIR/data/${DATA_TYPE}/${DATA_TYPE} -save_model $MODEL_DIR/${NAME} -encoder_type hybrid -rnn_size $RNN_SIZE -word_vec_size 123 -batch_size 8  -optim adadelta -dropout 0.1 -enc_layers 4 -dec_layers $DEC_LAYERS -learning_rate_decay 0.99 -epochs 5 -train_with_aug $TRAIN_WITH_AUG -num_concat_flags $NUM_CONCAT_FLAGS -add_noise $ADD_NOISE -use_highway_concat $HIGHWAY_CONCAT -mix_factor $MIX_FACTOR  -mix_factor_decay 0.95
