89126d5850324cac5e67c006879c16bac0a96d04
arendu <adithya.r@gmail.com>
2018-02-16 11:52:03 -0500

    updated test

--- test.sh
+++ test.sh
@@ -9,7 +9,11 @@ AUG_TYPE=$3
 export PYTHONPATH=$PYTHONPATH:$OPENMNTPATH/onmt/modules
 
 MODEL_DIR=$PROJECT_DIR/models/${DATA_TYPE}/${AUG_TYPE}
+OUTPUTS_DIR=$PROJECT_DIR/outputs/${DATA_TYPE}/${AUG_TYPE}
+mkdir -p $OUTPUTS_DIR
+
 #looking for single word folder/best
 #python $OPENMNTPATH/translate.py -model $MODEL_DIR/best -src $PROJECT_DIR/data/${DATA_TYPE}/${DATA_TYPE}.${AUG_TYPE}.audio.${DATA_SECT}.src -vocab $PROJECT_DIR/data/${DATA_TYPE}/${DATA_TYPE}.${AUG_TYPE}.vocab.pt -verbose -output $PROJECT_DIR/data/${DATA_TYPE}/${DATA_TYPE}.${AUG_TYPE}.audio.${DATA_SECT}.out -batch_size 1 -beam_size 12
-python $OPENMNTPATH/translate.py -model $MODEL_DIR/best -src $PROJECT_DIR/data/${DATA_TYPE}/${AUG_TYPE}/${DATA_TYPE}.audio.${DATA_SECT}.src -verbose -output $PROJECT_DIR/data/${DATA_TYPE}/${AUG_TYPE}/${DATA_TYPE}.audio.${DATA_SECT}.out -batch_size 1 -beam_size 12
+python $OPENMNTPATH/translate.py -model $MODEL_DIR/best -src $PROJECT_DIR/data/${DATA_TYPE}/${AUG_TYPE}/${DATA_TYPE}.audio.${DATA_SECT}.src -verbose -output $OUTPUTS_DIR/${DATA_SECT}.out -batch_size 1 -beam_size 12
+python $OPENMNTPATH/tools/wer.py -p $OUTPUTS_DIR/${DATA_SECT}.out -r $PROJECT_DIR/data/${DATA_TYPE}/${AUG_TYPE}/${DATA_TYPE}.audio.${DATA_SECT}.tgt > $OUTPUTS_DIR/${DATA_SECT}.out.wer  
 #python $OPENMNTPATH/translate.py -model /export/b18/shuoyangd/projects/asr/models/timit/best -src /export/b18/shuoyangd/projects/asr/data/timit/timit.audio.valid.src  -verbose -output tmp.out -batch_size 1 -beam_size 12
